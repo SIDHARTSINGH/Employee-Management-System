aaf02caa4cf118e94ed31f38f26f3b4c38ded284
Sidhart Singh <isidhartsingh@gmail.com>
2023-02-11 23:31:39 +0530

    added onClick() events on form and SaveEmployee()

--- employee-system-api/src/main/java/com/singh/sidhart/employee/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/singh/sidhart/employee/controller/EmployeeController.java
@@ -3,13 +3,11 @@ package com.singh.sidhart.employee.controller;
 import com.singh.sidhart.employee.model.Employee;
 import com.singh.sidhart.employee.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:3000")
 public class EmployeeController {
     @Autowired
     private final EmployeeService employeeService;
--- employee-system-ui/src/components/AddEmployee.js
+++ employee-system-ui/src/components/AddEmployee.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
+
+  const [employee, setEmployee] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    emailId: ""
+  })
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setEmployee({...employee, [e.target.name]: value})
+  };
+
+  const saveEmployee = (e) => {
+    // stop the webpage from refreshing on click
+    e.preventDefault();
+
+    EmployeeService.saveEmployee(employee)
+                   .then((response) => {
+						console.log(response);
+				   }).catch((error) => {
+						console.log(error);
+				   })
+
+  }
 
   return (
     <div className="flex max-w-2xl mx-auto shadow border-b">
@@ -15,6 +41,8 @@ const AddEmployee = () => {
           <input
             type="text"
             name="firstName"
+            value={employee.firstName}
+            onChange={(e) => handleChange(e)}
             className="h-10 w-96 border mt-2 px-2 py-2"></input>
         </div>
         <div className="items-center justify-center h-14 w-full my-4">
@@ -24,6 +52,8 @@ const AddEmployee = () => {
           <input
             type="text"
             name="lastName"
+            value={employee.lastName}
+            onChange={(e) => handleChange(e)}
             className="h-10 w-96 border mt-2 px-2 py-2"></input>
         </div>
         <div className="items-center justify-center h-14 w-full my-4">
@@ -33,11 +63,14 @@ const AddEmployee = () => {
           <input
             type="email"
             name="emailId"
+            value={employee.emailId}
+            onChange={(e) => handleChange(e)}
             className="h-10 w-96 border mt-2 px-2 py-2"></input>
         </div>
 
         <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
           <button
+            onClick={saveEmployee}
             className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6">
             Save
           </button>
